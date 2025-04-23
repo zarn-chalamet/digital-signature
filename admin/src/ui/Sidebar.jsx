5be9267239ad5dcd25050070e5e15cb0017524ee
fisheeesh <swanphyo444@gmail.com>
2025-04-23 18:10:01 +0700

    ui: close modals with animations and adjust side bar styles

--- admin/src/ui/Sidebar.jsx
+++ admin/src/ui/Sidebar.jsx
@@ -4,18 +4,15 @@ import { NavLink } from 'react-router-dom';
 import { cn } from '@/utils/cn'
 import logoImage from '@/assets/logo.png'
 import { navbarLinks } from '@/utils/constants';
-import useTheme from '@/hooks/useTheme';
 
 const Sidebar = forwardRef(({ collapsed }, ref) => {
-    const { isDark } = useTheme()
     return (
         <aside
             ref={ref}
             className={cn(
-                'fixed z-[100] flex h-full w-[300px] flex-col overflow-x-hidden shadow-lg [transition:_width_300ms_cubic-bezier(0.4,_0,_0.2,_1),_left_300ms_cubic-bezier(0.4,_0,_0.2,_1),_background-color_150ms_cubic-bezier(0.4,_0,_0.2,_1),_border_150ms_cubic-bezier(0.4,_0,_0.2,_1)] ',
+                'fixed z-[100] dark:bg-slate-900 dark:border-slate-700 dark:border-r bg-white border-slate-300 border-r flex h-full w-[300px] flex-col overflow-x-hidden shadow-lg [transition:_width_300ms_cubic-bezier(0.4,_0,_0.2,_1),_left_300ms_cubic-bezier(0.4,_0,_0.2,_1),_background-color_150ms_cubic-bezier(0.4,_0,_0.2,_1),_border_150ms_cubic-bezier(0.4,_0,_0.2,_1)] ',
                 collapsed ? 'md:w-[100px] md:items-center ' : 'md:w-[280px]',
-                collapsed ? 'max-md:-left-full' : 'max-md:left-0',
-                isDark ? 'bg-slate-900 border-slate-700 border-r' : 'bg-white border-slate-300 border-r'
+                collapsed ? 'max-md:-left-full' : 'max-md:left-0'
             )}>
             <div className='flex flex-col items-center justify-center p-4 h-[70px]'>
                 <img src={logoImage} alt="doitung_logo" className='w-24' />
@@ -30,7 +27,7 @@ const Sidebar = forwardRef(({ collapsed }, ref) => {
                         )}>
                             {
                                 navbarLink.links.map((link, index) => (
-                                    <NavLink key={index} to={link.path} className={cn("sidebar-item", collapsed && "md:w-[45px]", isDark && 'text-slate-400')}>
+                                    <NavLink key={index} to={link.path} className={cn("sidebar-item", collapsed && "md:w-[45px]")}>
                                         <link.icon size={22} className="flex-shrink-0" />
                                         {
                                             !collapsed && <p className='whitespace-nowrap'>{link.label}</p>
--- admin/src/ui/modals/Modal.jsx
+++ admin/src/ui/modals/Modal.jsx
@@ -23,16 +23,27 @@ const Open = ({ children, opens: openWindowName }) => {
 
 const Window = ({ children, name, width, padding = true }) => {
     const { openName, close } = useContext(ModalContext)
+    const [isClosing, setIsClosing] = useState(false)
 
-    const modalRef = useOutsideClick(close)
+    const closeWithAnimation = () => {
+        setIsClosing(true)
+        setTimeout(() => {
+            setIsClosing(false)
+            close()
+        }, 300)
+    }
 
-    if (openName !== name) return null
+    const modalRef = useOutsideClick(() => {
+        if (!isClosing) closeWithAnimation()
+    })
+
+    if (openName !== name && !isClosing) return null
 
     return createPortal(
-        <div className="fixed z-[100] inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-xs animate-fadeIn">
-            <div ref={modalRef} className={`${width ? `md:w-[600px] w-[${width}]` : 'w-96'} ${padding && 'p-6'} bg-slate-50 dark:bg-slate-900 dark:border-slate-700 border dark:text-slate-50 rounded-lg shadow-lg animate-slideUp`}>
+        <div className={`fixed px-6 z-[100] inset-0 flex items-center bg-black justify-center drop-shadow-lg bg-opacity-50 backdrop-blur-xs transition-opacity duration-300 ${isClosing ? 'opacity-0' : 'opacity-100'}`}>
+            <div ref={modalRef} className={`${width ? `md:w-[600px] w-[${width}]` : 'w-96'} ${padding && 'p-6'} bg-slate-50 dark:bg-slate-900 dark:border-slate-700 border transition-transform duration-300 dark:text-slate-50 rounded-lg shadow-lg ${isClosing ? 'animate-slideDown' : 'animate-slideUp'}`}>
                 <div>
-                    {cloneElement(children, { onCloseModal: close })}
+                    {cloneElement(children, { onCloseModal: closeWithAnimation })}
                 </div>
             </div>
         </div>,
